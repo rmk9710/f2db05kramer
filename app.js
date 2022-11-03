7b5a445915e8e3badbb22d7adce1ed55add6d009
Reece Kramer <54494919+rmk9710@users.noreply.github.com>
2022-11-02 21:29:59 -0500

    part 6

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var onion = require('./routes/onion');
+var gridbuild = require('./routes/gridbuild');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/onion', onion);
+app.use('/gridbuild', gridbuild);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
