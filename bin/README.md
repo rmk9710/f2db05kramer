293a532c746e58b2b711c6fcec24de538ccf0b14
Reece Kramer <54494919+rmk9710@users.noreply.github.com>
2022-11-09 20:57:11 -0600

    second commit for part 4

--- bin/README.md
+++
@@ -1,4 +0,0 @@
-# f2wb05kramer
-server template for assignment 10
-link to render server: https://f2wb05kramer.onrender.com/
-my attribute: class Onion onion_cost(number), onion size(string), onion_color(string)
--- bin/routes/onion.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET onion page. */
-router.get('/', function(req, res, next) {
-  res.render('onion', { title: 'Search Results Onion' });
-});
-
-module.exports = router;
